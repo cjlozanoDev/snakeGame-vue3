c7ea6081f605567e2f2f72084434185f973f2e3e
cjlozanoDev <carlosjlp@gmail.com>
2023-08-16 19:13:43 +0200

    Add first logic move of head

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,102 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import BlockSnake from './components/BlockSnake.vue'
+import { onMounted, onUnmounted, ref, computed } from 'vue'
+
+const listBlockSnake = ref([
+  {
+    positionX: 200,
+    positionY: 0,
+    head: true
+  }
+])
+
+const directionMove = ref('right')
+const interval = ref(null)
+
+const sizeScreen = computed(() => ({
+  x: window.innerWidth,
+  y: window.innerHeight
+}))
+
+onMounted(() => {
+  window.addEventListener('keydown', logKey)
+  startInterval()
+})
+
+onUnmounted(() => {
+  clearInterval(interval.value)
+})
+
+const logKey = (e) => {
+  clearInterval(interval.value)
+  const left = 'ArrowLeft'
+  const up = 'ArrowUp'
+  const right = 'ArrowRight'
+  const down = 'ArrowDown'
+
+  if (e.code == up && directionMove.value !== 'up' && directionMove.value !== 'down') {
+    directionMove.value = 'up'
+  }
+  if (e.code == down && directionMove.value !== 'up' && directionMove.value !== 'down') {
+    directionMove.value = 'down'
+  }
+  if (e.code == left && directionMove.value !== 'left' && directionMove.value !== 'right') {
+    directionMove.value = 'left'
+  }
+  if (e.code == right && directionMove.value !== 'left' && directionMove.value !== 'right') {
+    directionMove.value = 'right'
+  }
+  startInterval()
+}
+
+const startInterval = () => {
+  interval.value = setInterval(moveInterval, 10)
+}
+const moveInterval = () => {
+  const positionX = listBlockSnake.value[0].positionX
+  const positionY = listBlockSnake.value[0].positionY
+
+  if (directionMove.value === 'right') {
+    if (positionX >= sizeScreen.value.x) {
+      listBlockSnake.value[0].positionX = 0
+    } else {
+      listBlockSnake.value[0].positionX += 2
+    }
+  }
+  if (directionMove.value === 'left') {
+    if (positionX <= 0) {
+      listBlockSnake.value[0].positionX = sizeScreen.value.x
+    } else {
+      listBlockSnake.value[0].positionX -= 2
+    }
+  }
+  if (directionMove.value === 'up') {
+    if (positionY <= 0) {
+      listBlockSnake.value[0].positionY = sizeScreen.value.y
+    } else {
+      listBlockSnake.value[0].positionY -= 2
+    }
+  }
+  if (directionMove.value === 'down') {
+    if (positionY >= sizeScreen.value.y) {
+      listBlockSnake.value[0].positionY = 0
+    } else {
+      listBlockSnake.value[0].positionY += 2
+    }
+  }
+}
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    <h1>Juego Snake</h1>
   </header>
 
   <main>
-    <TheWelcome />
+    <BlockSnake
+      :position-x="listBlockSnake[0].positionX"
+      :position-y="listBlockSnake[0].positionY"
+    />
   </main>
 </template>
 
@@ -26,12 +109,10 @@ header {
   display: block;
   margin: 0 auto 2rem;
 }
-
 @media (min-width: 1024px) {
   header {
     display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+    flex-direction: column;
   }
 
   .logo {
---
+++ src/components/BlockSnake.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { computed } from 'vue'
+const props = defineProps({
+  positionX: {
+    type: Number,
+    default: 0
+  },
+  positionY: {
+    type: Number,
+    default: 0
+  }
+})
+const styleBlock = computed(() => ({
+  position: 'absolute',
+  width: '10px',
+  height: '10px',
+  backgroundColor: '#ff0000',
+  transform: `translate(${props.positionX}px, ${props.positionY}px)`
+}))
+</script>
+
+<template>
+  <div :style="styleBlock" />
+</template>
